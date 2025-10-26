a0e3bde32d33ae7f83ec9345104fc8b9de47b56b
tsaruk53 <rostislavcaruk1@gmail.com>
2025-10-26 23:51:03 +0200

    Delete script1.js

--- script1.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const navList = document.querySelector('.nav ul');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  navList.classList.toggle('open');
-});
